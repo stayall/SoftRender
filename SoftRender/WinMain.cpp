a4c0c6de11fe369a2ff350b94bcde33cf19c8b10
asus <asus@LAPTOP-H84FBCEI>
2022-12-20 20:19:46 +0800

    添加项目文件。

---
+++ SoftRender/WinMain.cpp
@@ -0,0 +1,9 @@
+#include <Windows.h>
+
+
+INT WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    PSTR lpCmdLine, INT nCmdShow)
+{
+    const wchar_t ClASS_NAME = TEXT("RenderWindow")
+    return 0;
+}

f3eb0821e7d8aff3e6412078beb7c3a7dbad967b
wuching <mr.wuching@gmail.com>
2022-12-21 19:43:42 +0800

    tesy and debug

--- SoftRender/MainWindow.cpp
+++ SoftRender/MainWindow.cpp
@@ -5,25 +5,25 @@ PCWSTR MainWindow::ClassName() const
     return L"Class";
 }
 
-LRESULT MainWindow::HandleMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
+        HDC hdc = BeginPaint(m_hwnd, &ps);
 
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
-        EndPaint(hwnd, &ps);
+        EndPaint(m_hwnd, &ps);
         return 0;
     }
     case WM_CLOSE:
     {
-        if (MessageBox(hwnd, L"Really close", L"Application", MB_OKCANCEL) == IDOK)
+        if (MessageBox(m_hwnd, L"Really close", L"Application", MB_OKCANCEL) == IDOK)
         {
-            DestroyWindow(hwnd);
+            DestroyWindow(m_hwnd);
         }
         return 0;
     }
@@ -33,6 +33,6 @@ LRESULT MainWindow::HandleMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lP
         return 0;
     }
     default:
-        return DefWindowProc(hwnd, uMsg, wParam, lParam);;
+        return DefWindowProc(m_hwnd, uMsg, wParam, lParam);;
     }
 }
--- SoftRender/MainWindow.h
+++ SoftRender/MainWindow.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "BaseWindow.h"
 
-class MainWindow : BaseWindow<MainWindow>
+class MainWindow : public BaseWindow<MainWindow>
 {
+public:
 	
 	virtual PCWSTR ClassName() const override;
-	virtual LRESULT HandleMessage(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) override;
+	virtual LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) override;
 };
 

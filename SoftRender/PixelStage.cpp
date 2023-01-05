41c08096d334dc3f52f8718261823c52efdc2a3e
wuching <mr.wuching@gmail.com>
2023-01-05 23:01:30 +0800

    PixelStage and depth test by Z-buffer

--- SoftRender/PixelStage.cpp
+++ SoftRender/PixelStage.cpp
@@ -1,1 +1,63 @@
 #include "PixelStage.h"
+
+const float PixelStage::maxDepth = 1.0f;
+
+void PixelStage::SetDepthStencil(int w, int h)
+{
+	width = w;
+	height = h;
+
+	depthStencil.resize(width);
+	for (auto& v : depthStencil)
+	{
+		v.resize(height);
+	}
+	ClearDepthStencil();
+}
+
+bool PixelStage::DepthTest(const Fragment& f)
+{
+	auto &Depth = depthStencil[f.viewPosition.x][f.viewPosition.y];
+	if (f.position.z >= Depth)
+	{
+		Depth = f.position.z;
+	}
+	return false;
+}
+
+void PixelStage::ClearDepthStencil()
+{
+	for (auto& vs : depthStencil)
+	{
+		for (auto& v : vs)
+		{
+			v = maxDepth;
+		}
+	}
+}
+
+void PixelStage::GenerateFream(const std::vector<Fragment>& fragments)
+{
+	if (depthStencil.empty())
+	{
+		return;
+	}
+
+	fream.resize(width);
+	for (auto& v : fream)
+	{
+		v.resize(height);
+	}
+
+	for (const auto& f : fragments)
+	{
+		if (DepthTest(f))
+		{
+			PixelShading(f);
+		}
+	}
+}
+
+void PixelStage::PixelShading(const Fragment &f)
+{
+}
--- SoftRender/PixelStage.h
+++ SoftRender/PixelStage.h
@@ -3,11 +3,19 @@
 
 #include "VertexData.h"
 
+
 class PixelStage
 {
 public:
+	void SetDepthStencil(int width, int height);
+	bool DepthTest(const Fragment& f);
+	void ClearDepthStencil();
 	void GenerateFream(const std::vector<Fragment>& fragments);
+	void PixelShading(const Fragment &f);
 private:
-
+	static const float maxDepth;
+	int width, height;
+	std::vector<std::vector<float>> depthStencil;
+	std::vector<std::vector<float>> fream;
 };
 
